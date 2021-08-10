e6e8d4b6345debf437be86c14a6722b992e476f9
1032231418 <zhangbch@txxy.com>
2021-08-10 16:12:58 +0800

    config.yaml.example

--- main.go
+++ main.go
@@ -10,16 +10,22 @@ import (
 	"os"
 )
 
-//const defaultConfigFile = "/usr/lib/zabbix/alertscripts/config.yaml"
-const defaultConfigFile = "config.yaml"
-
 var (
 	GVA_CONFIG config.Server
 )
 
 func main() {
+	//接受参数
+	args := os.Args //获取用户输入的所有参数
+	//args的类型是[]string
+
+	configfile := args[1]
+	t := args[2]       //获取输入的第一个参数
+	subject := args[3] //获取输入的第二个参数
+	body := args[4]    //获取输入的第二个参数
+
 	v := viper.New()
-	v.SetConfigFile(defaultConfigFile)
+	v.SetConfigFile(configfile)
 	err := v.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
@@ -34,14 +40,6 @@ func main() {
 	if err := v.Unmarshal(&GVA_CONFIG); err != nil {
 		fmt.Println(err)
 	}
-
-	//接受参数
-	args := os.Args //获取用户输入的所有参数
-	//args的类型是[]string
-
-	t := args[1]       //获取输入的第一个参数
-	subject := args[2] //获取输入的第二个参数
-	body := args[3]    //获取输入的第二个参数
 
 	switch {
 	case t == "mail":
